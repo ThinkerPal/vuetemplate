235a7841c9ee40d80c5596385c40a81472b224c1
ThinkerPal <thinkerpal@gmail.com>
2019-03-11 17:13:21 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vuetemplate
-Created with CodeSandbox
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text" v-model="text" />
+    <button v-on:Click="addTodo">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoInput",
+  data() {
+    return {
+      text: ""
+    };
+  },
+
+  methods: {
+    addTodo() {
+      this.$emit("todo:add", this.text);
+      this.text = "";
+    }
+  }
+};
+</script>
